67e3de864989d8c1b79bbea5f10beb083975299f
Ollie Chick <och26@uclive.ac.nz>
2019-07-31 15:21:16 +1200

    Read vertices from file, remove elems, glDrawArrays instead of Elements, 16 control points instead of 4
    also make comments and title reflect that this is assignment 1

--- Scene.cpp
+++ Scene.cpp
@@ -1,11 +1,11 @@
 //  ========================================================================
 //  COSC422: Advanced Computer Graphics;  University of Canterbury.
 //
-//  FILE NAME: CubePatches.cpp
-//  See Ex-06.pdf for details.
+//  Assignment 1
 //
-//	The program generates and loads the mesh data for a cube (8 verts, 24 elems).
-//  Requires files  CubePatches.h, CubePatches.vert, CubePatches.frag
+//  Written by Ollie Chick
+//
+//  Adapted from COSC422 labs
 //  ========================================================================
 
 #include <iostream>
@@ -15,7 +15,6 @@
 #include <GL/freeglut.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-#include "Scene.h"
 using namespace std;
 
 GLuint vaoID;
@@ -27,133 +26,138 @@ float CDR = 3.14159265/180.0;   //Conversion from degrees to radians (required i
 
 GLuint loadShader(GLenum shaderType, string filename)
 {
-	ifstream shaderFile(filename.c_str());
-	if(!shaderFile.good()) cout << "Error opening shader file." << endl;
-	stringstream shaderData;
-	shaderData << shaderFile.rdbuf();
-	shaderFile.close();
-	string shaderStr = shaderData.str();
-	const char* shaderTxt = shaderStr.c_str();
+    ifstream shaderFile(filename.c_str());
+    if(!shaderFile.good()) {
+        cout << "Error opening shader file." << endl;
+    }
+    stringstream shaderData;
+    shaderData << shaderFile.rdbuf();
+    shaderFile.close();
+    string shaderStr = shaderData.str();
+    const char* shaderTxt = shaderStr.c_str();
 
-	GLuint shader = glCreateShader(shaderType);
-	glShaderSource(shader, 1, &shaderTxt, NULL);
-	glCompileShader(shader);
-	GLint status;
-	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
-	if (status == GL_FALSE)
-	{
-		GLint infoLogLength;
-		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
-		GLchar *strInfoLog = new GLchar[infoLogLength + 1];
-		glGetShaderInfoLog(shader, infoLogLength, NULL, strInfoLog);
-		cerr <<  "Compile failure in shader: " << strInfoLog << endl;
-		delete[] strInfoLog;
-	}
-	return shader;
+    GLuint shader = glCreateShader(shaderType);
+    glShaderSource(shader, 1, &shaderTxt, NULL);
+    glCompileShader(shader);
+    GLint status;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (status == GL_FALSE) {
+        GLint infoLogLength;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
+        GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+        glGetShaderInfoLog(shader, infoLogLength, NULL, strInfoLog);
+        cerr <<  "Compile failure in shader: " << strInfoLog << endl;
+        delete[] strInfoLog;
+    }
+    return shader;
 }
 
 
 void initialise()
 {
-	glm::mat4 proj, view;
-	GLuint shaderv = loadShader(GL_VERTEX_SHADER, "Scene.vert");
-	GLuint shaderc = loadShader(GL_TESS_CONTROL_SHADER, "Scene.cont");
-	GLuint shadere = loadShader(GL_TESS_EVALUATION_SHADER, "Scene.eval");
-	GLuint shaderf = loadShader(GL_FRAGMENT_SHADER, "Scene.frag");
+    glm::mat4 proj, view;
+    GLuint shaderv = loadShader(GL_VERTEX_SHADER, "Scene.vert");
+    GLuint shaderc = loadShader(GL_TESS_CONTROL_SHADER, "Scene.cont");
+    GLuint shadere = loadShader(GL_TESS_EVALUATION_SHADER, "Scene.eval");
+    GLuint shaderf = loadShader(GL_FRAGMENT_SHADER, "Scene.frag");
 
-	GLuint program = glCreateProgram();
-	glAttachShader(program, shaderv);
-	glAttachShader(program, shaderc);
-	glAttachShader(program, shadere);
-	glAttachShader(program, shaderf);
-	glLinkProgram(program);
+    GLuint program = glCreateProgram();
+    glAttachShader(program, shaderv);
+    glAttachShader(program, shaderc);
+    glAttachShader(program, shadere);
+    glAttachShader(program, shaderf);
+    glLinkProgram(program);
 
-	GLint status;
-	glGetProgramiv (program, GL_LINK_STATUS, &status);
-	if (status == GL_FALSE)
-	{
-		GLint infoLogLength;
-		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
-		GLchar *strInfoLog = new GLchar[infoLogLength + 1];
-		glGetProgramInfoLog(program, infoLogLength, NULL, strInfoLog);
-		fprintf(stderr, "Linker failure: %s\n", strInfoLog);
-		delete[] strInfoLog;
-	}
-	glUseProgram(program);
+    GLint status;
+    glGetProgramiv (program, GL_LINK_STATUS, &status);
+    if (status == GL_FALSE) {
+        GLint infoLogLength;
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infoLogLength);
+        GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+        glGetProgramInfoLog(program, infoLogLength, NULL, strInfoLog);
+        fprintf(stderr, "Linker failure: %s\n", strInfoLog);
+        delete[] strInfoLog;
+    }
+    glUseProgram(program);
 
 
-	proj = glm::perspective(20.0f*CDR, 1.0f, 10.0f, 1000.0f);  //perspective projection matrix
-	view = glm::lookAt(glm::vec3(0.0, 5.0, 12.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0)); //view matrix
-	projView = proj*view;  //Product matrix
+    proj = glm::perspective(20.0f*CDR, 1.0f, 10.0f, 1000.0f);  //perspective projection matrix
+    view = glm::lookAt(glm::vec3(0.0, 5.0, 12.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0)); //view matrix
+    projView = proj*view;  //Product matrix
 
-	GLuint vboID[4];
+    //Read coordinates from file
+    ifstream infile;
+    infile.open("PatchFiles/PatchVerts_Gumbo.txt", ios::in);
+    int nvert;
+    infile >> nvert;
+    float verts[nvert * 3];
+    for(int i = 0; i < nvert; i++) {
+        infile >> verts[3*i] >> verts[3*i + 1] >> verts[3*i + 2];
+    }
+    infile.close();
 
-	glGenVertexArrays(1, &vaoID);
+    GLuint vboID[4];
+
+    glGenVertexArrays(1, &vaoID);
     glBindVertexArray(vaoID);
 
-    glGenVertexArrays(2, vboID);
+    glGenVertexArrays(1, vboID);
 
     glBindBuffer(GL_ARRAY_BUFFER, vboID[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
     glEnableVertexAttribArray(0);  // Vertex position
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboID[1]);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elems), elems, GL_STATIC_DRAW);
-
     glBindVertexArray(0);
 
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);
-	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 }
 
 void update(int value)
 {
-	angle++;
-	glutTimerFunc(50, update, 0);
-	glutPostRedisplay();
+    angle++;
+    glutTimerFunc(50, update, 0);
+    glutPostRedisplay();
 }
 
 void display()
 {
-	glm::mat4 matrix = glm::mat4(1.0);
-	matrix = glm::rotate(matrix, angle*CDR, glm::vec3(0.0, 1.0, 0.0));  //rotation matrix
-	glm::mat4 prodMatrix = projView * matrix;        //Model-view-proj matrix
+    glm::mat4 matrix = glm::mat4(1.0);
+    matrix = glm::rotate(matrix, angle*CDR, glm::vec3(0.0, 1.0, 0.0));  //rotation matrix
+    glm::mat4 prodMatrix = projView * matrix;        //Model-view-proj matrix
 
-	glUniformMatrix4fv(matrixLoc, 1, GL_FALSE, &prodMatrix[0][0]);
+    glUniformMatrix4fv(matrixLoc, 1, GL_FALSE, &prodMatrix[0][0]);
 
-	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-	glBindVertexArray(vaoID);
-	glDrawElements(GL_PATCHES, 24, GL_UNSIGNED_SHORT, NULL);
-    glPatchParameteri(GL_PATCH_VERTICES, 4);
-	glFlush();
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glBindVertexArray(vaoID);
+    glDrawArrays(GL_PATCHES, 0, 2048);
+    glPatchParameteri(GL_PATCH_VERTICES, 16);
+    glFlush();
 }
 
 int main(int argc, char** argv)
 {
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB|GLUT_DEPTH);
-	glutInitWindowSize(500, 500);
-	glutCreateWindow("Cube with Bezier patches");
-	glutInitContextVersion (4, 2);
-	glutInitContextProfile ( GLUT_CORE_PROFILE );
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB|GLUT_DEPTH);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("COSC422: Ollie Chick");
+    glutInitContextVersion (4, 2);
+    glutInitContextProfile ( GLUT_CORE_PROFILE );
 
-	if(glewInit() == GLEW_OK)
-	{
-		cout << "GLEW initialization successful! " << endl;
-		cout << " Using GLEW version " << glewGetString(GLEW_VERSION) << endl;
-	}
-	else
-	{
-		cerr << "Unable to initialize GLEW  ...exiting." << endl;
-		exit(EXIT_FAILURE);
-	}
+    if(glewInit() == GLEW_OK) {
+        cout << "GLEW initialization successful! " << endl;
+        cout << " Using GLEW version " << glewGetString(GLEW_VERSION) << endl;
+    } else {
+        cerr << "Unable to initialize GLEW  ...exiting." << endl;
+        exit(EXIT_FAILURE);
+    }
 
-	initialise();
-	glutDisplayFunc(display);
-	glutTimerFunc(50, update, 0); 
-	glutMainLoop();
+    initialise();
+    glutDisplayFunc(display);
+    glutTimerFunc(50, update, 0);
+    glutMainLoop();
 }
 
